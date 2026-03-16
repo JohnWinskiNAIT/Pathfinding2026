314e99e7973e28a15976ff0a0de03fa305f9e986
JohnWinskiNAIT <47360288+JohnWinskiNAIT@users.noreply.github.com>
2026-03-16 11:09:44 -0600

    Add project files.

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] GameObject startNode, destNode, currentNode, targetNode, prevNode;
+    [SerializeField] List<GameObject> targets;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        transform.position = startNode.transform.position;
+        currentNode = startNode;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get reference distance
+        float closestDist = 100000;
+
+        // Get list of connections
+        currentNode.GetComponent<Pathnode>().GetConnections(ref targets);
+
+        // Find closest connection
+        for (int i = 0; i < targets.Count; i++)
+        {
+            if (Vector3.Distance(destNode.transform.position, targets[i].transform.position) < closestDist && targets[i] != prevNode)
+            {
+                targetNode = targets[i];
+                closestDist = Vector3.Distance(destNode.transform.position, targets[i].transform.position);
+            }
+            
+        }
+
+        // Move to target
+        if (targetNode != null)
+        {
+            targetNode.GetComponent<Pathnode>().nodeActive = false;
+            currentNode.GetComponent<Pathnode>().nodeActive = true;
+            transform.Translate((targetNode.transform.position - transform.position).normalized * Time.deltaTime * 3);
+        }
+
+        // Switch current node when target reached
+        if (Vector3.Distance(transform.position, targetNode.transform.position) < 0.15f)
+        {            
+            prevNode = currentNode;
+            currentNode = targetNode;            
+        }
+    }
+}
---
+++ Assets/Scripts/Pathnode.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathnode : MonoBehaviour
+{
+    public List<GameObject> connections;
+
+    public bool nodeActive = true;
+
+    Pathnode()
+    {
+        connections = new List<GameObject>();
+
+        nodeActive = true;
+    }
+
+    public void AddConnection(GameObject target)
+    {
+        connections.Add(target);
+    }
+
+    public void ClearConnections()
+    {
+        connections.Clear();
+    }
+
+    public void GetConnections(ref List<GameObject> targets)
+    {
+        targets = connections;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+
+        foreach (GameObject target in connections)
+        {
+            if (target.GetComponent<Pathnode>().nodeActive == true)
+            {
+                Gizmos.color = Color.green;
+            }
+            else
+            {
+                Gizmos.color = Color.red;
+            }
+            
+            Gizmos.DrawLine(transform.position, target.transform.position + new Vector3(0, 0.5f, 0));
+        }
+    }
+}
